77a4f2fba23cc4dfdcecb19da254e1d1fca5b9f7
Jonathan Wu <jwu20@stuy.edu>
2020-11-24 00:07:28 -0500

    added RationalNumber java and constructor

---
+++ RationalNumber.java
@@ -0,0 +1,15 @@
+public class RationalNumber extends RealNumber {
+  private int numerator, denominator;
+
+  public RationalNumber(int nume, int deno) {
+    super(0.0);
+    if (deno == 0) {
+      nume = 0;
+      deno = 1;
+    } else {
+      numerator = nume;
+      denominator = deno;
+    }
+  }
+  
+}
