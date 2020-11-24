43b10dbedfdee443c9b0f7165fbf277ee22f7c81
Jonathan Wu <jwu20@stuy.edu>
2020-11-24 00:19:44 -0500

    added RationalNumber gcd method

--- RationalNumber.java
+++ RationalNumber.java
@@ -39,4 +39,21 @@ public class RationalNumber extends RealNumber {
   public String toString() {
     return "" + numerator + "/" + denominator;
   }
+
+  public static int gcd(int a, int b) {
+    if (a < b) {
+      int tempValue = b;
+      b = a;
+      a = b;
+    }
+    int remainders = a / b;
+    for (int i = 0; remainders == 0; i++) {
+      a = b;
+      b = remainders;
+      remainders = a / b;
+    }
+    if (remainders == 0) {
+      System.out.println("" + remainders + " is the GCD of " + a + " and " + b);
+    }
+  }
 }
