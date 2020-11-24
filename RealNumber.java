d2f3c88298de25b16f199e56ab6d952c267b17d5
Jonathan Wu <jwu20@stuy.edu>
2020-11-24 00:04:30 -0500

    added add, multiply, divide and subtract methods

--- RealNumber.java
+++ RealNumber.java
@@ -14,4 +14,36 @@ public class RealNumber {
   }
 
 //---------ONLY EDIT BELOW THIS LINE------------
+
+  public boolean equals(RealNumber other) {
+    double differencePercent = ((other - value) / value);
+    if ((value == 0) && (other == 0)) {
+      return true;
+    }
+    else if (differencePercent <= 0.001) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public RealNumber add(RealNumber other) {
+    value = value + other;
+    return null;
+  }
+
+  public RealNumber multiply(RealNumber other) {
+    value = value * other;
+    return null;
+  }
+
+  public RealNumber divide(RealNumber other) {
+    value = value / other;
+    return null;
+  }
+
+  public RealNumber subtract(RealNumber other) {
+    value = value - other;
+    return null;
+  }
 }
