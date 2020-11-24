e20ead96d92774f905c6f8f452851b51b8f93a7a
Jonathan Wu <jwu20@stuy.edu>
2020-11-24 00:10:36 -0500

    added RationalNumber reciprocal method

--- RationalNumber.java
+++ RationalNumber.java
@@ -23,4 +23,8 @@ public class RationalNumber extends RealNumber {
   public int getDenominator() {
     return denominator;
   }
+
+  public RationalNumber reciprocal() {
+    return (denominator / numerator);
+  }
 }
