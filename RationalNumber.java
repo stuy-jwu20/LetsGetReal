714567f52ff889e4f4d85037fe639714ca71c62b
Jonathan Wu <jwu20@stuy.edu>
2020-11-24 00:09:26 -0500

    added RationalNumber getNumerator

--- RationalNumber.java
+++ RationalNumber.java
@@ -15,4 +15,8 @@ public class RationalNumber extends RealNumber {
   public double getValue() {
     return (numerator / denominator);
   }
+
+  public int getNumerator() {
+    return numerator;
+  }
 }
